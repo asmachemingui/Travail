e925b4723d51a861b11338032aaad6b4d3a4ac6e
Chemingui Asma <Chemingui.Asma@issatso.u-sousse.tn>
2018-11-12 21:28:34 +0100

    Dirst

---
+++ EBoutique/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using EBoutique.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EBoutique.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public new ActionResult User()
+        {
+            return View();
+        }
+        public ActionResult Tables()
+        {
+            return View();
+        }
+        public JsonResult GetArticles()
+        {
+            iBoutiqureDBEntities dc = new iBoutiqureDBEntities();
+            List<ArticleViewModel> articles = dc.Articles.Select(x => new ArticleViewModel
+            {
+                idArticle = x.idArticle,
+                refArticle = x.refArticle,
+                libelleArticle = x.libelleArticle,
+                prix = x.prix,
+                description = x.description,
+                disponibilite = x.disponibilite,
+                nbpieces = x.nbpieces,
+                libelleMarque = x.Marque.libelleMarque,
+                libelleType = x.Type.libelleType,
+                libelleCatgorie = x.Categorie.libelleCatgorie,
+                couleur = x.couleur,
+                taille = x.taille
+            }
+            ).ToList();
+            return Json(new { data = articles }, JsonRequestBehavior.AllowGet);
+        }
+
+
+    }
+}
---
+++ EBoutique/EBoutique/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EBoutique.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult User()
+        {
+            return View();
+        }
+        public ActionResult Tables()
+        {
+            return View();
+        }
+    }
+}
